c46b069284c87d4d9b2088f77eedfaa28a86d844
Afrikanochka <Mtgnatali@gmail.com>
2021-08-13 16:14:37 +0300

    copy hw-08

---
+++ src/views/HomeView/HomeView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import s from './HomeView.module.css';
+
+const HomeView = () => {
+  return <h1 className={s.heading}>Welcome! Your Phonebook is ready to go!</h1>;
+};
+
+export default HomeView;
